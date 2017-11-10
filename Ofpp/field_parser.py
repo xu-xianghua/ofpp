d40e6ed97ea6925ae0cd3a22c4a00482fececa6d
XU Xianghua <dayigu@gmail.com>
2017-11-10 10:23:22 +0800

    add binary format support

--- Ofpp/field_parser.py
+++ Ofpp/field_parser.py
@@ -3,11 +3,10 @@ field_parser.py
 parser for field data
 
 """
-from __future__  import print_function
+from __future__ import print_function
 
-import re
 import os
-import io
+import struct
 import numpy as np
 
 
@@ -20,7 +19,7 @@ def parse_field_all(fn):
     if not os.path.exists(fn):
         print("Can not open file " + fn)
         return None
-    with io.open(fn, encoding="utf-8") as f:
+    with open(fn, "rb") as f:
         content = f.readlines()
         return parse_internal_field_content(content), parse_boundary_content(content)
 
@@ -34,7 +33,7 @@ def parse_internal_field(fn):
     if not os.path.exists(fn):
         print("Can not open file " + fn)
         return None
-    with io.open(fn, encoding="utf-8") as f:
+    with open(fn, "rb") as f:
         content = f.readlines()
         return parse_internal_field_content(content)
 
@@ -45,13 +44,12 @@ def parse_internal_field_content(content):
     :param content: contents of lines
     :return: numpy array of internal field
     """
+    is_binary = is_binary_format(content)
     for ln, lc in enumerate(content):
-        if lc.startswith('internalField'):
-            if 'nonuniform' in lc:
-                if 'vector' in lc or 'tensor' in lc:
-                    return parse_data_nonuniform(content, ln, True)
-                return parse_data_nonuniform(content, ln, False)
-            elif 'uniform' in lc:
+        if lc.startswith(b'internalField'):
+            if b'nonuniform' in lc:
+                return parse_data_nonuniform(content, ln, len(content), is_binary)
+            elif b'uniform' in lc:
                 return parse_data_uniform(content[ln])
             break
     return None
@@ -66,7 +64,7 @@ def parse_boundary_field(fn):
     if not os.path.exists(fn):
         print("Can not open file " + fn)
         return None
-    with io.open(fn, encoding="utf-8") as f:
+    with open(fn, "rb") as f:
         content = f.readlines()
         return parse_boundary_content(content)
 
@@ -78,21 +76,22 @@ def parse_boundary_content(content):
     :return:
     """
     data = {}
+    is_binary = is_binary_format(content)
     bd = split_boundary_content(content)
     for boundary, (n1, n2) in bd.items():
         pd = {}
         n = n1
         while True:
             lc = content[n]
-            if 'nonuniform' in lc:
-                if 'vector' in lc or 'tensor' in lc:
-                    v = parse_data_nonuniform(content, n, True)
-                else:
-                    v = parse_data_nonuniform(content, n, False)
+            if b'nonuniform' in lc:
+                v = parse_data_nonuniform(content, n, n2, is_binary)
                 pd[lc.split()[0]] = v
-                n += len(v) + 4
+                if not is_binary:
+                    n += len(v) + 4
+                else:
+                    n += 3
                 continue
-            elif 'uniform' in lc:
+            elif b'uniform' in lc:
                 pd[lc.split()[0]] = parse_data_uniform(content[n])
             n += 1
             if n > n2:
@@ -107,35 +106,41 @@ def parse_data_uniform(line):
     :param line: a line include uniform data, eg. "value           uniform (0 0 0);"
     :return: data
     """
-    p = re.compile(r'uniform\s*\((.*)\)')
-    ps = p.search(line)
-    if ps is not None:
-        return np.fromstring(ps.group(1))
-    p = re.compile(r'uniform\s*(.*);')
-    ps = p.search(line)
-    if ps is not None:
-        try:
-            v = float(ps.group(1))
-            return v
-        except:
-            return None
-    else:
-        return None
+    if b'(' in line:
+        return np.array([float(x) for x in line.split(b'(')[1].split(b')')[0].split()])
+    return float(line.split(b'uniform')[1].split(b';')[0])
 
 
-def parse_data_nonuniform(content, n, is_vector):
+def parse_data_nonuniform(content, n, n2, is_binary):
     """
     parse nonuniform data from lines
     :param content: data content
     :param n: line number
-    :param is_vector: data is vector or not
+    :param n2: last line number
+    :param is_binary: binary format or not
     :return: data
     """
     num = int(content[n + 1])
-    if is_vector:
-        data = np.array([ln[1:-2].split() for ln in content[n + 3:n + 3 + num]], dtype=float)
+    if not is_binary:
+        if b'scalar' in content[n]:
+            data = np.array([float(x) for x in content[n + 3:n + 3 + num]])
+        else:
+            data = np.array([ln[1:-2].split() for ln in content[n + 3:n + 3 + num]], dtype=float)
     else:
-        data = np.array([float(x) for x in content[n + 3:n + 3 + num]])
+        nn = 1
+        if b'vector' in content[n]:
+            nn = 3
+        elif b'symmtensor' in content[n]:
+            nn = 6
+        elif b'tensor' in content[n]:
+            nn = 9
+        buf = b''.join(content[n+2:n2+1])
+        vv = np.array(struct.unpack('{}d'.format(num*nn),
+                                    buf[struct.calcsize('c'):num*nn*struct.calcsize('d')+struct.calcsize('c')]))
+        if nn > 1:
+            data = vv.reshape((num, nn))
+        else:
+            data = vv
     return data
 
 
@@ -152,34 +157,34 @@ def split_boundary_content(content):
     current_path = ''
     while True:
         lc = content[n]
-        if lc.startswith('boundaryField'):
+        if lc.startswith(b'boundaryField'):
             in_boundary_field = True
-            if content[n+1].startswith('{'):
+            if content[n+1].startswith(b'{'):
                 n += 2
                 continue
-            elif content[n+1].strip() == '' and content[n+2].startswith('{'):
+            elif content[n+1].strip() == b'' and content[n+2].startswith(b'{'):
                 n += 3
                 continue
             else:
                 print('no { after boundaryField')
                 break
         if in_boundary_field:
-            if lc.startswith('}'):
+            if lc.rstrip() == b'}':
                 break
             if in_patch_field:
-                if lc.strip() == '}':
+                if lc.strip() == b'}':
                     bd[current_path][1] = n-1
                     in_patch_field = False
                     current_path = ''
                 n += 1
                 continue
-            if lc.strip() == '':
+            if lc.strip() == b'':
                 n += 1
                 continue
             current_path = lc.strip()
-            if content[n+1].strip() == '{':
+            if content[n+1].strip() == b'{':
                 n += 2
-            elif content[n+1].strip() == '' and content[n+2].strip() == '{':
+            elif content[n+1].strip() == b'' and content[n+2].strip() == b'{':
                 n += 3
             else:
                 print('no { after boundary patch')
@@ -195,3 +200,17 @@ def split_boundary_content(content):
 
     return bd
 
+
+def is_binary_format(content, maxline=20):
+    """
+    parse file header to judge the format is binary or not
+    :param content: file content in line list
+    :param maxline: maximum lines to parse
+    :return: binary format or not
+    """
+    for lc in content[:maxline]:
+        if b'format' in lc:
+            if b'binary' in lc:
+                return True
+            return False
+    return False
