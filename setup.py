5d039daca4b25fa80925267559351be745ec0bb9
Jan Drees <jdrees@mailbox.org>
2019-09-05 16:38:40 +0200

    Renaming the source folder to be consistent with the new project name

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.12',  # Required
+    version='0.13',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -192,6 +192,7 @@ setup(
     # what's used to render the link text on PyPI.
     project_urls={  # Optional
         'Parent Project Description': 'https://www.forschung-it-sicherheit-kommunikationssysteme.de/projekte/mamoko',
+        'Source': 'https://github.com/ApolloLV/openfoamparser',
         'Forked from': 'https://github.com/dayigu/ofpp',
     },
 )
